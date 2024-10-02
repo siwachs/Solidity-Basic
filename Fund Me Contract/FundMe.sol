5b547a42ce3bdccce4f2346187342af608915745
siwachs <ssiwach.8888@gmail.com>
2024-10-02 18:31:08 +0530

    Basic Contract Finish

--- Fund Me Contract/FundMe.sol
+++ Fund Me Contract/FundMe.sol
@@ -7,38 +7,64 @@ contract FundMe {
     using PriceConverter for uint256;
 
     // Use Chainlink and oracle to convert USD to ETH
-    uint256 public minimumUSD = 1 * 1e18;
+    uint256 public minimumUSD;
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
+    address public owner;
+
+    constructor() {
+        minimumUSD = 1 * 1e18;
+
+        // Is going to be the owner who deploy this contract
+        owner = msg.sender;
+    }
+
     function fund() public payable {
-        require(msg.value.getConversionRate() >= minimumUSD, "Minimum 50USD need to for this transaction!");
+        require(
+            msg.value.getConversionRate() >= minimumUSD,
+            "Minimum 50USD need to for this transaction!"
+        );
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
     }
 
-    function withdraw() public {
-       for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
-        address funder =  funders[funderIndex];
-        addressToAmountFunded[funder] = 0;
-       }
+    function withdraw() public onlyOwner {
+        // require(msg.sender == owner, "Only owner can withdraw these ETH!");
+        // Or better use Modifiers
 
-        // Reset Array start at index 0    
-       funders = new address[](0);
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
 
-       // Three way to send ETH -> transfer, send, call
+        // Reset Array start at index 0
+        funders = new address[](0);
 
-       // Use 2300 GAS and throw Error
-       // payable(msg.sender).transfer(address(this).balance);
+        // Three way to send ETH -> transfer, send, call
 
-       // use 2300 GAS and return bool
-       // bool sendSuccess = payable(msg.sender).send(address(this).balance);
-       // require(sendSuccess,"Fail to transfer ETH.");
+        // Use 2300 GAS and throw Error
+        // payable(msg.sender).transfer(address(this).balance);
 
-       // call A lower level command use all the GAS or use all avail GAS
-       // Use memory because bytes object are array - bytes memory dataReturned
-       (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
-       require(callSuccess,"Fail to transfer ETH.");
+        // use 2300 GAS and return bool
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess,"Fail to transfer ETH.");
+
+        // call A lower level command use all the GAS or use all avail GAS
+        // Use memory because bytes object are array - bytes memory dataReturned
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Fail to transfer ETH.");
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can withdraw these ETH!");
+        _; // This symbol indicates where the rest of the function code will execute after the require statement
     }
 }
