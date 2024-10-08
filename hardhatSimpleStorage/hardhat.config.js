fb84dd6f0017be66b18ddf39692786c47fee17c8
siwachs <ssiwach.8888@gmail.com>
2024-10-08 14:51:35 +0530

    .

--- hardhatSimpleStorage/hardhat.config.js
+++ hardhatSimpleStorage/hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const TESTNET_RCP_URL = process.env.TESTNET_RCP_URL;
+const TESTNET_PRIVATE_KEY = process.env.TESTNET_PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    testnet: {
+      url: TESTNET_RCP_URL,
+      accounts: [TESTNET_PRIVATE_KEY],
+      chainId: 11155111, //ETH Testnet
+    },
+  },
   solidity: "0.8.19",
 };
